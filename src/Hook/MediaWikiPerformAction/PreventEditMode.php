12019921e2cdd0fcf313ece998fb80d7b989416a
dvogel <daniel.vogel@hallowelt.com>
2020-05-08 09:14:03 +0200

    "New page" dialog or "new subpage" dialog will direct enter edit mode if pagetemplates is not available.
    ERM18842
    
    [3.1.x]
    
    Change-Id: Ied0c8d8b2049ea9989e3bf0e39d372878fa27c8a

---
+++ src/Hook/MediaWikiPerformAction/PreventEditMode.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace BlueSpice\PageTemplates\Hook\MediaWikiPerformAction;
+
+use Article;
+use BSPageTemplateList;
+use MediaWiki;
+use MediaWiki\MediaWikiServices;
+use OutputPage;
+use Title;
+use User;
+use WebRequest;
+
+class PreventEditMode {
+	protected static $editActions = [ 'edit', 'sourceedit', 'formedit' ];
+
+	/**
+	 * @param OutputPage $output
+		* @param Article $article
+		* @param Title $title
+		* @param User $user
+		* @param WebRequest $request
+		* @param MediaWiki $wiki
+		* @return bool
+	 */
+	public static function callback( $output, $article, $title, $user, $request, $wiki ) {
+		if ( $title->exists() ) {
+			return true;
+		}
+
+		if ( isset( $request->getQueryValues()['preload'] ) ) {
+			return true;
+		}
+
+		$action = $request->getVal( 'action', $request->getVal( 'veaction', null ) );
+		if ( !$action || !in_array( $action, static::$editActions ) ) {
+			return true;
+		}
+
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		$excludeNs = $config->get( 'PageTemplatesExcludeNs' );
+		if ( in_array( $title->getNamespace(), $excludeNs ) ) {
+			return true;
+		}
+
+		$templateList = new BSPageTemplateList( $title );
+		$availableTemplates = $templateList->getAll();
+		if ( count( $availableTemplates ) === 1 && isset( $availableTemplates[-1] ) ) {
+			// index -1 is "Empty page"
+			$emptyPageTemplate = $availableTemplates[-1];
+			$targetUrl = static::ensurePreloadParam( $emptyPageTemplate['target_url'] );
+			$output->redirect( $targetUrl );
+		} else {
+			$output->redirect( $title->getLocalURL( [ 'action' => 'view' ] ) );
+		}
+
+		return true;
+	}
+
+	/**
+	 * Make sure that "preload" param is present in the URL
+	 * to prevent infinite loops
+	 *
+	 * @param string $target
+	 * @return string
+	 */
+	private static function ensurePreloadParam( $target ) {
+		if ( strpos( $target, 'preload=' ) === false ) {
+			return wfAppendQuery( $target, [ 'preload' => '' ] );
+		}
+
+		return $target;
+	}
+}
