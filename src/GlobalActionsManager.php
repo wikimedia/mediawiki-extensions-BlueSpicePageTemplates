f423763663ebb48c19a7667813c489e9ff20e44c
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 15:08:50 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: I438940cbd67ec671315c1585ba744bf84b5cc123

---
+++ src/GlobalActionsManager.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BlueSpice\PageTemplates;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsManager extends RestrictedTextLink {
+
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-pagetemplates';
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'pagetemplatesadmin-viewspecialpage'
+		];
+		return $permissions;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'PageTemplatesAdmin' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-pagetemplates-desc' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'pagetemplatesadmin' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'pagetemplatesadmin' );
+	}
+}
