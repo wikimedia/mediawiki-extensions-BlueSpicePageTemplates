88791263fa48a071e29e0a790b4001e74e6f58b4
akulbii <alk@astarios.com>
2022-07-01 14:05:08 +0300

    Add standard page templates
    Moved out from here:
    https://gerrit.wikimedia.org/r/c/mediawiki/extensions/BlueSpiceDistributionConnector/+/809313
    Decided to split general fetch/import mechanism and specifically delivering templates (as soon as they need records in DB, but regular pages don't).
    
    [4.2.x]
    
    ERM28466
    
    Change-Id: I7480fbe3a7e9c29fb408c4ebcd80ada103d04b69
    (cherry picked from commit 79df2f43022869bf9f13937f072f16110452d358)
    (cherry picked from commit 13b37a77de45ebfa7bd211ca16715858bda4c1cb)

---
+++ src/ContentImport/PageTemplateDAO.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace BlueSpice\PageTemplates\ContentImport;
+
+use Wikimedia\Rdbms\IDatabase;
+
+class PageTemplateDAO {
+
+	/**
+	 * Database connection object (DB_PRIMARY with write access needed)
+	 *
+	 * @var IDatabase
+	 */
+	private $db;
+
+	/**
+	 * @param IDatabase $db Database connection object (DB_PRIMARY with write access needed)
+	 */
+	public function __construct( IDatabase $db ) {
+		$this->db = $db;
+	}
+
+	/**
+	 * @param string $title
+	 * @param int $namespace
+	 *
+	 * @return bool
+	 */
+	public function templateExists( string $title, int $namespace ): bool {
+		$conds = [
+			'pt_template_title' => $title,
+			'pt_template_namespace' => $namespace
+		];
+
+		$isTemplatePersisted = $this->db->selectField(
+			'bs_pagetemplate',
+			'pt_id',
+			$conds
+		);
+
+		return $isTemplatePersisted;
+	}
+
+	/**
+	 * @param string $label
+	 * @param string $description
+	 * @param string $title
+	 * @param string $namespace
+	 *
+	 * @return void
+	 */
+	public function insertTemplate( string $label, string $description, string $title, string $namespace ): void {
+		$set = [
+			'pt_label' => $label,
+			'pt_desc' => $description,
+			'pt_target_namespace' => json_encode( [ -99 ] ),
+			'pt_template_title' => $title,
+			'pt_template_namespace' => $namespace
+		];
+
+		$this->db->insert(
+			'bs_pagetemplate',
+			$set
+		);
+	}
+
+	/**
+	 * @param string $label
+	 * @param string $description
+	 * @param string $title
+	 * @param string $namespace
+	 *
+	 * @return void
+	 */
+	public function updateTemplate( string $label, string $description, string $title, string $namespace ): void {
+		$conds = [
+			'pt_template_title' => $title,
+			'pt_template_namespace' => $namespace
+		];
+
+		$set = [
+			'pt_label' => $label,
+			'pt_desc' => $description,
+			'pt_target_namespace' => json_encode( [ -99 ] ),
+			'pt_template_title' => $title,
+			'pt_template_namespace' => $namespace
+		];
+
+		$this->db->update(
+			'bs_pagetemplate',
+			$set,
+			$conds
+		);
+	}
+}
