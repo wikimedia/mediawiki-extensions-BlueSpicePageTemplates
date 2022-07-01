13b37a77de45ebfa7bd211ca16715858bda4c1cb
akulbii <alk@astarios.com>
2022-07-01 14:05:08 +0300

    Add standard page templates
    Moved out from here:
    https://gerrit.wikimedia.org/r/c/mediawiki/extensions/BlueSpiceDistributionConnector/+/809313
    Decided to split general fetch/import mechanism and specifically delivering templates (as soon as they need records in DB, but regular pages don't).
    
    [4.2.x]
    
    ERM28466
    
    Change-Id: I7480fbe3a7e9c29fb408c4ebcd80ada103d04b69
    (cherry picked from commit 79df2f43022869bf9f13937f072f16110452d358)

---
+++ src/HookHandler/SetupDefaultTemplates.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BlueSpice\PageTemplates\HookHandler;
+
+use BlueSpice\PageTemplates\Maintenance\PostDatabaseUpdate\PageTemplatesImport;
+use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
+
+class SetupDefaultTemplates implements LoadExtensionSchemaUpdatesHook {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onLoadExtensionSchemaUpdates( $updater ) {
+		$updater->addPostDatabaseUpdateMaintenance(
+			PageTemplatesImport::class
+		);
+	}
+}
