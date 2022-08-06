472dffa4dbac705017519515ab17161e203c3eb5
akulbii <alk@astarios.com>
2022-08-06 21:09:35 +0300

    Add support for templates with multiple target namespaces
    There was incorrect behavior in case with template with multiple target namespaces.
    For example: we have “Template1” with target namespaces "NS1", “NS2” and “NS3”.
    We want to create new page “Test page” in main namespace.
    Config Force target namespace is set to true.
    So when we create specified page in main namespace, “Template1” is displayed 3 times, for each target namespace separately.
    But “target url” is the same for each case, “NS1:Test page”.
    So it does not matter which template we choose - in any case “NS1:Test page” will be created.
    
    [4.2.x]
    
    ERM18435
    
    Change-Id: Icf67f3a5d7884e2a9f530f3cf4ed7561d37791ff

--- src/ConfigDefinition/PageTemplatesForceNamespace.php
+++ src/ConfigDefinition/PageTemplatesForceNamespace.php
@@ -12,15 +12,9 @@ class PageTemplatesForceNamespace extends BooleanSetting {
 	 */
 	public function getPaths() {
 		return [
-			static::MAIN_PATH_FEATURE . '/' .
-			static::FEATURE_CONTENT_STRUCTURING .
-			'/BlueSpicePageTemplates',
-			static::MAIN_PATH_EXTENSION .
-			'/BlueSpicePageTemplates/' .
-			static::FEATURE_CONTENT_STRUCTURING,
-			static::MAIN_PATH_PACKAGE . '/' .
-			static::PACKAGE_FREE .
-			'/BlueSpicePageTemplates',
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_CONTENT_STRUCTURING . '/BlueSpicePageTemplates',
+			static::MAIN_PATH_EXTENSION . '/BlueSpicePageTemplates/' . static::FEATURE_CONTENT_STRUCTURING,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpicePageTemplates',
 		];
 	}
 
