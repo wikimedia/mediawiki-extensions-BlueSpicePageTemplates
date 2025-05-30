b51d1077080bc31b85ba6ed3460f398c939ccc18
d.savuljesku <d.savuljesku@gmail.com>
2025-05-30 09:07:02 +0200

    Refactor to use GenericTagHandler
    [5.1.x]
    
    ERM42208
    
    Change-Id: Iaad0715a3ecb2e3dd7b6406db928f5a25e5806d5
    (cherry picked from commit 71e4bd7eb0d46b4c699fe849ebe8b4b2a9f69b1e)

--- src/Tag/PageTemplates.php
+++ src/Tag/PageTemplates.php
@@ -2,55 +2,66 @@
 
 namespace BlueSpice\PageTemplates\Tag;
 
-use BlueSpice\Tag\MarkerType\NoWiki;
-use BlueSpice\Tag\Tag;
-use MediaWiki\Parser\Parser;
-use MediaWiki\Parser\PPFrame;
+use MediaWiki\MediaWikiServices;
+use MWStake\MediaWiki\Component\GenericTagHandler\ClientTagSpecification;
+use MWStake\MediaWiki\Component\GenericTagHandler\GenericTag;
+use MWStake\MediaWiki\Component\GenericTagHandler\ITagHandler;
+use MWStake\MediaWiki\Component\GenericTagHandler\MarkerType;
 
-class PageTemplates extends Tag {
-	/**
-	 * @param mixed $processedInput
-	 * @param array $processedArgs
-	 * @param Parser $parser
-	 * @param PPFrame $frame
-	 *
-	 * @return IHandler
-	 */
-	public function getHandler(
-		$processedInput,
-		array $processedArgs,
-		Parser $parser,
-		PPFrame $frame
-		) {
-		return new PageTemplatesHandler( $processedInput, $processedArgs, $parser, $frame );
-	}
+class PageTemplates extends GenericTag {
 
 	/**
-	 *
-	 * @return \ParamDefinition[]
+	 * @inheritDoc
 	 */
-	public function getArgsDefinitions() {
-		return [];
-	}
-
-	/**
-	 * @return string[]
-	 */
-	public function getTagNames() {
+	public function getTagNames(): array {
 		return [ 'bs:pagetemplates', 'pagetemplates' ];
 	}
 
 	/**
 	 * @return bool
 	 */
-	public function needsDisabledParserCache() {
-		return true;
+	public function hasContent(): bool {
+		return false;
 	}
 
 	/**
 	 * @inheritDoc
 	 */
-	public function getMarkerType() {
-		return new NoWiki();
+	public function getHandler( MediaWikiServices $services ): ITagHandler {
+		return new PageTemplatesHandler(
+			$services->getHookContainer(),
+			$services->getConfigFactory()
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getMarkerType(): MarkerType {
+		return new MarkerType\NoWiki();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getResourceLoaderModules(): ?array {
+		return [
+			'ext.bluespice.pageTemplates.tag',
+			'ext.bluespice.pageTemplates.styles'
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getParamDefinition(): ?array {
+		return null;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getClientTagSpecification(): ClientTagSpecification|null {
+		return null;
 	}
 }
